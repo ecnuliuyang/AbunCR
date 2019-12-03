daa1304f234ce0cd875990dab4c94869b8ee6897
Yang Liu <liuyangecnu@163.com>
2019-12-03 20:31:51 +0800

    Update fun.em.cc.R

--- R/fun.em.cc.R
+++ R/fun.em.cc.R
@@ -1,216 +1,194 @@
 #################################################################################
-###  Abundance estimation when covariates are subject to missing 
-###        under discrete time capture-recapture models 
-###  The naive empirical likelihood method
+###  The inverse probability weighting and multiple imputation methods
 #################################################################################
 
 #################################################################################
-###  1. Minimize   - sum.i  log(1+lambda t.i)  with respect to lambda                      
+###  2. Function to implement the multiple imputation method
 #################################################################################
 
-el.lagrange <- function(t){
-  
-  ### Function to approximate log(t)
-  logg <- function(t){
-    
-    eps <- 1e-5
-    ts <- t*(t>eps)+1*(t<=eps)
-    log(ts) * (t>eps) + (log(eps) - 1.5 + 2*t/eps - (t/eps)^2*0.5) * (t<=eps)
-    
+mi2.mar <- function(d, K, x = NULL, y, level = 0.95, M = 100, seed = 321, beta.initial = NULL){
+
+  ##############################################################################
+  ############   Preparation  ################
+
+  y.mat <- as.matrix(y)
+  na.loc <- apply(y.mat, 1, function(y) any(is.na(y)))
+  y.obs <- as.matrix(y.mat[!na.loc,])
+
+  d <- as.numeric(d)
+  n <- length(d)
+  m <- sum(!na.loc)
+  d.obs <- d[!na.loc]
+  d.mis <- d[na.loc]
+  d.all <- c( d.obs, d.mis)
+
+  if ( is.null(x) ) x.mat <- matrix(1, nrow=n)
+  else  x.mat <- as.matrix(x)
+
+  x.mis <- matrix(x.mat[na.loc,], ncol = ncol(x.mat))
+  x.obs <- matrix(x.mat[!na.loc,], ncol = ncol(x.mat))
+  x.all <- rbind(x.obs, x.mis)
+
+
+  ### z.obs = z.mat
+  z.obs = cbind(x.obs, y.obs)
+  set.seed(321)
+  if (is.null(beta.initial)) beta.initial <- runif( ncol(z.obs), -1, 1 )
+
+  p <- ncol(z.obs)
+  z.mat <- as.matrix(z.obs[, beta.initial!=0])
+  cov.all <- rbind(z.mat, cbind(x.mis, rep(0, n-m))
+                   [, beta.initial!=0] )
+  cov.all <- as.matrix(cov.all)
+
+  ind.obs <- c( rep(1, m), rep(0, n-m) ) ### r
+
+  data <- cbind(d.all, x.all, c(y.obs, rep(0, n-m)), c(rep(1, m), rep(0, n-m)) )
+  data <- as.matrix(data)
+  y.max <- max(y.obs)
+
+
+  ##############################################################################
+  ############   Main functions  ################
+
+  MI.data.fun <- function(){
+
+    mi.data <- matrix(0, n, M+1)
+    colnames(mi.data) <- c( paste0('Z',1:M), 'R' )
+    mi.data[, M+1] <- data[, ncol(data)]
+    for (i in 1:n)
+      if (data[i, ncol(data)] == 1) {
+        mi.data[i, 1:M] <- rep(y.obs[i], M)
+      }else{
+
+        xx <- y.obs[ d.obs == d.all[i] & apply( x.obs, 1, function(x) all(x == x.all[i,]) ) ]
+        if (length(xx) != 0) mi.data[i, 1:M] <- sample(xx, M, replace = T)
+        else mi.data[i,1:M] <- rep( mean(y.obs), M)
+
+      }
+
+
+    mi.data
   }
-  
-  emplik <- function(lam)  - sum( logg( 1 + lam*t ) )
-  
-  optimize(f=emplik, interval=c(-1e3, 1e3))
-}
 
+  like.con.mi <- function(beta){
 
-#################################################################################
-###  2. Minimize  - h.1(N,alpha) with respect to N for the given alpha    
-#################################################################################
+    beta <- as.matrix(beta)
+    like.i <- function(d, p.i, p.i.star)
+      d*log(p.i + 1e-300) + (K - d)*log(1 - p.i + 1e-300) - log(p.i.star + 1e-300)
 
-slike.part1 <- function(n, alpha){
-  
-  fgamma <- function(nt) - sum( log((nt - n + 1):nt) ) - (nt - n) * log(alpha+1e-20)  
-  
-  optimize(fgamma, interval=c(n, 100*n), tol=0.01)
-  
-}
+    out <- 0
+    for (mm in 1:M) {
+      dat.m[[mm]][,ncol(dat.m[[mm]])] <- dat.m[[mm]][,ncol(dat.m[[mm]])]/y.max
+      p.i <- plogis( dat.m[[mm]]%*%beta )
+      p.i.star <- 1 - (1 - p.i)^K
+      out <- out + sum( like.i(d.all, p.i, p.i.star) )
 
-#################################################################################
-###  3. Minimize -h.23(beta,alpha) with respect to beta for the given alpha 
-#################################################################################
-
-slike.part23 <- function(z.mat, d.obs, K, beta.init, alpha){
-  
-  z.mat <- as.matrix(z.mat)
-  
-  fun23 <- function(beta){
-    
-    g.beta <- as.numeric( plogis(z.mat%*%beta) )
-    phi <- (1 - g.beta)^K
-    result.lagrange <- el.lagrange( phi - alpha )
-    
-    tt <- sum( d.obs * log(g.beta + 1e-300) + (K - d.obs) * log(1 - g.beta + 1e-300) )
-    
-    - ( result.lagrange$objective + tt )
-    
-  }
-  
-  gr <- function(beta){
-    
-    g.beta <- as.numeric( plogis(z.mat%*%beta) )
-    phi <- (1-g.beta)^K
-    result.lagrange <- el.lagrange( phi - alpha )
-    lam <- result.lagrange$minimum
-    temp <- ( lam * alpha - 1 ) / ( 1 + lam * (phi - alpha) ) * K * g.beta
-    
-    - t(z.mat) %*% (temp + d.obs)
-    
-  }
-  
-  nlminb(beta.init, fun23, gradient=gr)
-  
-}
-
-
-#################################################################################
-###  4. Calculate empirical likelihood function for the given N.0  
-#################################################################################
-sel.null <- function(z.mat, d.obs, K, beta.init, n.big0){
-  
-  n <- length(d.obs)
-  falpha.null <- function(alpha){
-    
-    tmp1 <- - sum( log((n.big0 - n + 1):n.big0) ) - (n.big0 - n) * log(alpha)
-    tmp2 <- slike.part23(z.mat, d.obs, K, beta.init, alpha)$objective
-    tmp1 + tmp2
-  }
-  
-  eps <- 1e-5
-  out <- optimize(falpha.null, interval=c(eps, 1-eps), tol=eps)
-  - out$objective
-}
-
-
-#################################################################################
-###  5. Implement empirical likelihood method with no missing data   
-#################################################################################
-###  note: first column of z.mat is a vector with elements being 1.
-el.opt.cc <- function(z, d, K, CI = TRUE, level = .95,
-                   beta.initial = rep(0, ncol(as.matrix(z)))){
-  
-  z.mat <- as.matrix(z)
-  d.obs <- as.numeric(d)
-  n <- length(d.obs)
-  falpha <- function(alpha){
-    
-    tmp1 <- slike.part1(n, alpha)$objective
-    tmp2 <- slike.part23(z.mat, d.obs, K, beta.initial, alpha)$objective
-    tmp1 + tmp2
-  }
-  
-  eps <- 1e-5
-  out <- optimize(falpha, interval=c(eps, 1-eps), tol=eps)
-  like <- - out$objective
-  alpha.est <- out$minimum
-  
-  ##############  EL estimator of N  ##############
-  
-  out1 <- slike.part1(n, alpha.est)
-  n.big.est <- out1$minimum
-  
-  #################################################
-  ############## EL estimator of beta  ############
-  
-  out23 <- slike.part23(z.mat, d.obs, K, beta.initial, alpha.est)
-  beta.est <- out23$par
-  
-  #################################################
-  
-  
-  ###########   probability weights of EL  ##############
-  
-  g.beta <- as.numeric( plogis(z.mat %*% beta.est) )
-  phi <- (1 - g.beta)^K
-  lam <- el.lagrange(phi - alpha.est)$minimum
-  prob.est <- 1 / ( n*(1+lam*(phi-alpha.est)) )
-  
-  ###########      Test of convergence     ##############
-  converge <- ifelse( abs(1 - sum(prob.est) ) < 1e-2 & all(prob.est > 0) & all(prob.est < 1), 0, 1 )
-  
-  #### Maximum empirical log-likelihood and AIC #######
-  log.like <- sum( log((n.big.est - n + 1):n.big.est) ) - sum(log(1:n)) + 
-    (n.big.est - n) * log(alpha.est) + sum(log(prob.est)) +
-    sum( d.obs * log(g.beta + 1e-300) + (K - d.obs) * log(1 - g.beta + 1e-300) )
-  AIC <- 2*(-log.like + length(beta.est) + 1 + 1)
-  
-  ###########   ELR based CI  ##############
-  
-  rn <- function(nu){
-    
-    like.full <- like
-    like.null <- sel.null(z.mat, d.obs, K, beta.est, nu)
-    2 * ( like.full - like.null) - qchisq(level, 1)
-    
-  }
-  
-  if (CI) {
-    ntemp <- n.big.est
-    while(rn(ntemp) <= 0) ntemp <- ntemp*2
-    
-    ci.upper <- uniroot(rn, c(n.big.est, ntemp))$root
-    if (rn(n) <= 0) {
-      ci.lower <- n
-    }else{
-      ci.lower <- uniroot(rn, c(n, n.big.est))$root
     }
-    
-    ci.est <- c(ci.lower, ci.upper)
-  } else ci.est <- NULL
-  
-  
-  ###########   Variance estimates  ##############
-  dim_q=ncol(z.mat)
-  V23 = rep(0, dim_q)
-  V22= matrix(0,dim_q,dim_q)
-  
-  beta=beta.est
-  phi=(1-g.beta)^K
-  n.big.est = n.big.est
-  phistar=sum(1/(1-phi+1e-300)^2)/n.big.est
-  
-  V23.coef=(phi/(1-phi+1e-300)^2)*K*g.beta
-  V22.coef=(d.obs-K*g.beta/(1-phi+1e-300))^2
-  V22.part2=z.mat
-  
-  for(i in 1:n)
-  {
-    
-    V23=V23+V23.coef[i]*z.mat[i,]
-    qxi=matrix(z.mat[i,],ncol=1)
-    
-    V22=V22+V22.coef[i]*qxi%*%t(qxi)
-    
+    - out/M
   }
-  
-  V23=V23/n.big.est;
-  V22=-V22/n.big.est
-  V23=matrix(V23,ncol=1)
-  V32=t(V23)
-  V22.inv=solve(V22-diag(1e-300,dim_q,dim_q));
-  tmp = V32%*%V22.inv%*%V23
-  tmp=as.numeric(tmp)
-  
-  sigma_hat =  phistar - 1 - tmp
-  
-  list(n.big.est = n.big.est, ci.est = ci.est, 
-       alpha.est = alpha.est, beta.est = beta.est,
-       like = log.like, AIC = AIC,
-       converge = converge,
-       se.nu = sqrt(sigma_hat*n.big.est), 
-       se.beta = sqrt(diag(- V22.inv)/n.big.est))
-  
+
+  df.2 <- function(beta0){
+
+    out <- 0
+    for(mm in 1:M){
+      xx <- dat.m[[mm]]
+      p.i <- plogis(xx%*%beta0)
+      p.i.star <- 1 - (1-p.i)^K
+      item <- - as.numeric( (K * p.i * (1-p.i) * p.i.star - K^2 * p.i^2 * (1-p.i.star) )/(p.i.star^2) )
+      out <- out + t(xx) %*% diag(item) %*% xx
+    }
+    out/M
+  }
+
+  N.est.2 <- function(beta){
+
+    p.i.star.mi <- 0
+
+    for (mm in 1:M) {
+      p.i <- plogis( dat.m[[mm]]%*%beta )
+      p.i.star <- 1 - (1 - p.i)^K
+      p.i.star.mi <- p.i.star.mi + 1/p.i.star
+    }
+    sum(1/(M/p.i.star.mi))
+  }
+
+  var.est.2 <- function(beta){
+
+    # beta <- beta.est
+    GG <- - df.2(beta)
+
+    u.nu.i <- 0; u.ni <- 0; v.1 <- 0; v.3 <- 0; nu.est.mi <- NULL
+    for (mm in 1:M) {
+
+      xx <- dat.m[[mm]]
+      p.i <- plogis(xx%*%beta)
+      p.i.star <- 1 - (1 - p.i)^K
+      u.nu.i <- u.nu.i + t(xx) %*% diag(as.numeric( (d.all - K*p.i/p.i.star)^2 )) %*% xx
+      u.ni <- u.ni + t(xx) %*% (d.all - K*p.i/p.i.star) %*% t( t(xx) %*% (d.all - K*p.i/p.i.star) )
+      v.1 <- v.1 + sum( (1-p.i.star)/p.i.star^2 )
+      v.3 <- v.3 - t(xx) %*% ( K*(1 - p.i.star)*p.i/(p.i.star^2) )
+      nu.est.mi <- c( nu.est.mi, sum(1/p.i.star) )
+
+    }
+
+    vv <- solve(GG) %*% (u.nu.i/M + (1+1/M) * u.ni / (M-1)) %*% solve(GG)
+
+    c( diag(vv), v.1/M + (1+1/M) * sum( (nu.est.mi - est.mi[length(est.mi)])^2 )/(M-1) + t(v.3/M) %*% vv %*% (v.3/M) )
+
+  }
+
+  set.seed(seed)
+  dat.m <- list()
+
+  mi.data <- MI.data.fun()
+  for (mm in 1:M)
+    dat.m[[mm]] <- cbind(x.all, mi.data[,mm])[, beta.initial!=0]
+
+  out <- nlminb(beta.initial[beta.initial!=0], like.con.mi)
+  beta.est <- out$par
+  beta.est[length(beta.est)] <- beta.est[length(beta.est)]/y.max
+
+  est.mi <- c(beta.est, N.est.2(beta.est))
+  est.var <- var.est.2( beta.est )
+
+  rt <- list(n.big = est.mi[length(est.mi)],
+             n.big.se = sqrt(est.var[length(est.var)]),
+             n.big.ci = c( est.mi[length(est.mi)] - sqrt(est.var[length(est.var)])*qnorm(0.5+level/2),
+                           est.mi[length(est.mi)] + sqrt(est.var[length(est.var)])*qnorm(0.5+level/2) ),
+             beta = est.mi[1:(length(est.mi) - 1)],
+             beta.se =  sqrt(est.var[1:(length(est.var)-1)]),
+             level = level)
+  class(rt) <- 'MI'
+  return(rt)
+
 }
+
+
+print.MI <- function (obj) {
+
+  cat ("\nMultiple Imputation method: \n")
+  cat ("\nCoefficients:\n")
+  beta <- matrix(round(obj$beta, 2),nrow=1)
+  coefficients <- rbind( beta, round(obj$beta.se[1:ncol(beta)], 2) )
+  rownames(coefficients) <- c('Estimate','Std. Error')
+  colnames(coefficients) <- paste0('beta', 1:ncol(coefficients))
+
+  print(coefficients)
+
+  cat ("\nAbundance:\n")
+  cat ("Estimate:", round(obj$n.big))
+  cat ("\nStd. Error:", round(obj$n.big.se))
+
+  cat ("\n")
+  cat (paste0(100*obj$level,"%"),
+       "CI: [",
+       paste(round(obj$n.big.ci), collapse=', '), "]\n")
+
+
+}
+
+
+
 
 
